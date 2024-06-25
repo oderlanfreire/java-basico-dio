64002b5a140fc7e9ddd8e42718d737d98cffa8fe
Oderlan <oderlanfreire@gmail.com>
2024-06-25 11:41:22 -0300

    concluido

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,6 +1,44 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        System.out.println(nomeCompleto);
+        //concatenação de palavras
+        // String nomeCompleto = "LINGUAGEM" + "JAVA";
+        // System.out.println(nomeCompleto);
+
+        //concatenação de strings + inteiros (execução em modo debug)
+        // String concatenacao = "?";
+        
+        // concatenacao = 1 + 1 + 1 + "1";
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1 + "1" + 1 + 1;
+        // System.out.println(concatenacao);
+
+        // concatenacao = 1 + "1" + 1 + "1";
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1" + 1 + 1 + 1;
+        // System.out.println(concatenacao);
+
+        // concatenacao = "1" + (1 + 1 + 1);
+        // System.out.println(concatenacao);
+
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(- numero);
+
+        System.out.println(numero);
+
+        numero = + numero;
+
+        System.out.println(numero);
+
+
+        numero = numero * (-1);
+        System.out.println(numero);
+
+
+
     }
 }
